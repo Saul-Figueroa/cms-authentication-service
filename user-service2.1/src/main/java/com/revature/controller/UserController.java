56eb672cdde5a1346d933160db21dc8c7cceda34
Saul <sauleff@gmail.com>
2019-08-05 14:45:19 -0400

    Removing security

--- user-service2.1/src/main/java/com/revature/controller/UserController.java
+++ user-service2.1/src/main/java/com/revature/controller/UserController.java
@@ -1,6 +1,7 @@
 package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,6 +19,13 @@ public class UserController {
 	@Autowired
 	public UserController(UserService userService) {
 		this.userService=userService;
+	}
+	
+	
+	@GetMapping("test")
+	public String hello() {
+		
+		return "hello";
 	}
 	
 	@PostMapping(value="/login")
