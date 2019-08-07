1ffde03cccd9f3660200a6d7984504b4ad0195d7
Saul <sauleff@gmail.com>
2019-08-07 10:02:19 -0400

    updating

--- user-service2.1/src/main/java/com/revature/controller/UserController.java
+++ user-service2.1/src/main/java/com/revature/controller/UserController.java
@@ -1,4 +1,4 @@
-package com.revature.controller;
+	package com.revature.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -33,8 +33,6 @@ public class UserController {
 	@PostMapping(value="/login")
 	public User login(@RequestBody User user) {
 
-
-		
 		return userService.Authenticate(user);
 
 
